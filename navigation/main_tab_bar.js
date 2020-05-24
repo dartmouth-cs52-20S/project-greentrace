79a253520494ca0b8a4c11476b2c69349cc4463f
Hershel Wathore <wathoreh@gmail.com>
2020-05-24 16:12:38 -0400

    base map finished

--- navigation/main_tab_bar.js
+++ navigation/main_tab_bar.js
@@ -3,6 +3,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Ionicons from 'react-native-vector-icons/FontAwesome';
 import Map from '../components/map';
+import HeatMap from '../components/HeatMap';
 import Risk from '../components/risk';
 import Alerts from '../components/alerts';
 import Status from '../components/status';
@@ -33,7 +34,8 @@ const MainTabBar = () => {
           },
         })}
       >
-        <Tab.Screen name="Map" component={Map} />
+        <Tab.Screen name="Map" component={HeatMap} />
+        <Tab.Screen name="Map2" component={Map} />
         <Tab.Screen name="Risk" component={Risk} />
         <Tab.Screen name="Alerts" component={Alerts} />
         <Tab.Screen name="Status" component={Status} />
