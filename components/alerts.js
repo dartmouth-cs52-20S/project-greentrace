3d0cc4cf7f351d016f909c2fb417211d69bfb1c8
samisamosa <samihadatta@gmail.com>
2020-05-24 15:54:52 -0400

    empty state too

--- components/alerts.js
+++ components/alerts.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import {
-  StyleSheet, View, Text, TouchableOpacity, ScrollView,
+  // StyleSheet,
+  View,
+  Text,
+  TouchableOpacity,
+  ScrollView,
+  ActivityIndicator,
 } from 'react-native';
 import { connect } from 'react-redux';
 import Ionicons from 'react-native-vector-icons/FontAwesome';
 import { FlatList } from 'react-native-gesture-handler';
 import getDateUSFormatString from '../lib/date-lib';
+import { fetchMessages } from '../services/api';
 
 class Alerts extends Component {
   constructor(props) {
@@ -13,6 +19,28 @@ class Alerts extends Component {
     this.state = {
       isLoading: true,
     };
+  }
+
+  componentDidMount() {
+    // eslint-disable-next-line react/destructuring-assignment
+    this.props.fetchMessages();
+    this.setState({ isLoading: false });
+  }
+
+  renderLoadingView() {
+    return (
+      <View>
+        <ActivityIndicator size="large" color="#0000ff" />
+      </View>
+    );
+  }
+
+  renderEmptyState() {
+    return (
+      <View>
+        <Text>No alerts yet! :(</Text>
+      </View>
+    );
   }
 
   renderMessageThumbnail(message) {
@@ -29,7 +57,7 @@ class Alerts extends Component {
     }
 
     return (
-      <TouchableOpacity style={styles.thumbnail}>
+      <TouchableOpacity>
         <Ionicons name="warning" />
         <View>
           <Text>{text}</Text>
@@ -44,30 +72,33 @@ class Alerts extends Component {
     const { isLoading } = this.state;
     if (isLoading) {
       return this.renderLoadingView();
+    } else if (messages.length == 0) {
+      return this.renderEmptyState();
+    } else {
+      return (
+        <ScrollView>
+          <FlatList
+            data={messages}
+            renderItem={({ item }) => { return this.renderMessageThumbnail(item); }}
+            keyExtractor={(item) => item.timestamp}
+            // style={styles.listView}
+          />
+        </ScrollView>
+      );
     }
-    return (
-      <ScrollView style={styles.container}>
-        <FlatList
-          data={messages}
-          renderItem={({ item }) => { return this.renderMessageThumbnail(item); }}
-          keyExtractor={(item) => item.timestamp}
-          style={styles.listView}
-        />
-      </ScrollView>
-    );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'space-around',
-    alignItems: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     justifyContent: 'space-around',
+//     alignItems: 'center',
+//   },
+// });
 
 const mapStateToProps = (reduxState) => ({
   messages: reduxState.messages.messages,
 });
 
-export default connect(mapStateToProps, null)(Alerts);
+export default connect(mapStateToProps, { fetchMessages })(Alerts);
