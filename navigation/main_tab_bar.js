46b869ed7941b7ea07accf8591887439fa781f56
Hershel Wathore <wathoreh@gmail.com>
2020-05-25 19:11:46 -0400

    Map initalizes at current location

--- navigation/main_tab_bar.js
+++ navigation/main_tab_bar.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Ionicons from 'react-native-vector-icons/FontAwesome';
-import Map from '../components/map';
+// import Map from '../components/map';
 import HeatMap from '../components/HeatMap';
 import Risk from '../components/risk';
 import Alerts from '../components/alerts';
@@ -35,7 +35,6 @@ const MainTabBar = () => {
         })}
       >
         <Tab.Screen name="Map" component={HeatMap} />
-        <Tab.Screen name="Map2" component={Map} />
         <Tab.Screen name="Risk" component={Risk} />
         <Tab.Screen name="Alerts" component={Alerts} />
         <Tab.Screen name="Status" component={Status} />
