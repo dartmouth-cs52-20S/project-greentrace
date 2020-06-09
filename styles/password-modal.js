70be3b35eab6cdeed609326c2376eb7ef25dd65c
samisamosa <samihadatta@gmail.com>
2020-06-09 14:22:19 -0400

    styles?

---
+++ styles/password-modal.js
@@ -0,0 +1,45 @@
+import { StyleSheet } from 'react-native';
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
+
+const styles = StyleSheet.create({
+  error: {
+    fontSize: 10,
+    color: 'red',
+  },
+  textInput: {
+    borderColor: 'black',
+    borderWidth: 1,
+    borderRadius: 5,
+    width: wp('66%'),
+    height: hp('4.5%'),
+    padding: hp('1%'),
+    marginTop: hp('2.25%'),
+  },
+  container: {
+    backgroundColor: 'white',
+    alignSelf: 'center',
+    padding: 30,
+    borderRadius: 10,
+    flex: 1,
+    flexDirection: 'column',
+    width: 300,
+    maxHeight: 400,
+  },
+  field: {
+    padding: hp('1%'),
+  },
+  fieldTitle: {
+    fontSize: 15,
+    textAlign: 'left',
+  },
+  title: {
+    fontSize: 20,
+    textAlign: 'center',
+    marginVertical: 20,
+  },
+  button: {
+    marginTop: 15,
+  },
+});
+
+export default styles;
